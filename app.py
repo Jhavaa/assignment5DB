343c0cbbbc1ba5ea99b00b4cbc70d08a88d21e9d
Jhavaa <Jramisetty@ufl.edu>
2019-12-10 23:26:42 -0500

    Added templates: flowers.html (will hold list of flowers) and home.html (home page). Added sighting class to manage sightings table (currently only saves attributes).

--- app.py
+++ app.py
@@ -1,16 +1,38 @@
-# app.py
-from flask import Flask           # import flask
+from flask import Flask
+from flask import render_template
+from flask import request
 import os
 import sqlite3
-import pandas as pd
+from sighting import Sighting
 
-# Create a database
+# Create/Connect a database
 conn = sqlite3.connect('flowers2019.db')
 
-app = Flask(__name__)             # create an app instance
+# Create cursor to execute SQL commands
+c = conn.cursor()
 
-@app.route("/")                   # at the end point /
-def hello():                      # call method hello
-    return "Hello World!"         # which returns "hello world"
+# sight_1 = Sighting('', '', '', '')
+#
+# c.execute("INSERT INTO Sightings Values (:name, :person, :location, :sighted)", {'name': sight_1.name, 'person': sight_1.person, 'location': sight_1.location, 'sighted': sight_1.sighted})
+
+# Select all entries in the table Flowers from 'flowers2019.db'
+c.execute("SELECT * FROM Flowers")
+
+# fetch all entries returned by the SQL query
+print(c.fetchall())
+
+# Save query
+conn.commit()
+
+conn.close()
+
+# create an app instance
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])                # at the end point /
+def home():                      # call method home
+    if request.form:
+        print(request.form)
+    return render_template("home.html")       # which returns 'home.html' template
 if __name__ == "__main__":        # on running python app.py
     app.run()                     # run the flask app
---
+++ sighting.py
@@ -0,0 +1,10 @@
+class Sighting:
+    def __init__(self, name, person, location, sighted):
+        self.name = name
+        self.person = person
+        self.location = location
+        self.sighted = sighted
+
+    # @property
+    # def email(self):
+    #     return '{}.{}@email.com'.format(self.first, self.last)
